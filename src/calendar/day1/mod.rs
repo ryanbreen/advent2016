8c6f976c126cf4aed3b4090e124661d67ba247b6
Ryan Breen <ryan@ryanbreen.com>
2016-12-02 14:05:41 -0500

    Day 1, part 2.

--- src/calendar/day1/mod.rs
+++ src/calendar/day1/mod.rs
@@ -1,4 +1,6 @@
 
+use std::collections::HashSet;
+
 fn part1 (input: String) -> String  {
 
   // 0 is north, 1 is east, 2 is south, 3 is west
@@ -41,7 +43,56 @@ fn part1 (input: String) -> String  {
 }
 
 fn part2 (input: String) -> String  {
-  return input;
+    // 0 is north, 1 is east, 2 is south, 3 is west
+  let mut dir:i8 = 0;
+
+  let mut location:(i16, i16) = (0, 0);
+  let mut seen_locations = HashSet::new();
+
+  let parts:Vec<&str> = input.split(", ").collect();
+
+  let mut final_location:(i16, i16) = location;
+
+  'outer: for instruction in &parts {
+
+    match instruction.chars().next() {
+      Some('L') => dir -= 1,
+      Some('R') => dir += 1,
+      _ => panic!("Invalid input")
+    };
+
+    if dir < 0 {
+      dir = 3;
+    }
+
+    if dir > 3 {
+      dir = 0;
+    }
+
+    let distance_str:String = String::from(*instruction).chars().skip(1).collect();
+    let distance:i16 = distance_str.parse::<i16>().unwrap();
+
+    for _ in 0..distance {
+
+      match dir {
+        0 => { location.1 += 1 },
+        1 => { location.0 += 1 },
+        2 => { location.1 -= 1 },
+        3 => { location.0 -= 1 },
+        _ => { println!("Invalid direction") }
+      }
+
+      if seen_locations.contains(&location) {
+        println!("Visited twice {:?}", location);
+        final_location = location;
+        break 'outer;
+      }
+
+      seen_locations.insert(location);
+    }
+  }
+
+  return (final_location.0.abs() + final_location.1.abs()).to_string();
 }
 
 pub fn fill() -> super::Day {
