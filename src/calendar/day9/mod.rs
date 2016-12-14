699c10bb1cbf3fc63f69125e1eb1aa733da952bf
Ryan Breen <ryan@ryanbreen.com>
2016-12-13 22:21:55 -1000

    Day 9, part 2.

--- src/calendar/day9/mod.rs
+++ src/calendar/day9/mod.rs
@@ -1,5 +1,5 @@
 
-fn part1(input: String) -> String  {
+fn process_parts(input: String) -> String {
   let original:Vec<char> = input.chars().collect();
   let mut new:Vec<char> = vec!();
 
@@ -16,7 +16,6 @@ fn part1(input: String) -> String  {
         }
 
         i += 1; /* eat the final paren of this marker */
-
 
         let marker_parts = marker.split("x").collect::<Vec<&str>>();
         let data_block_len = marker_parts[0].parse::<usize>().unwrap();
@@ -37,11 +36,60 @@ fn part1(input: String) -> String  {
     };
   }
 
-  new.len().to_string()
+  new.iter().cloned().collect::<String>()
 }
 
-fn part2 (_: String) -> String  {
-  "CFLELOYFCS".to_string()
+fn part1(input: String) -> String  {
+  process_parts(input).len().to_string()
+}
+
+/**
+ * Return the size of a subset of the final string.  If any data regions are contained in this
+ * subset, we recursively traverse them.
+ */
+fn size_of_substring(input: String) -> usize {
+  let original:Vec<u8> = input.bytes().collect();
+
+  let mut my_size = 0;
+
+  let mut i = 0;
+
+  while i < original.len() {
+    match original[i] {
+      40 /* ( */ => {
+        i += 1;
+        let mut marker:String = String::new();
+        while original[i] != 41 {
+          marker.push(original[i] as char);
+          i += 1;
+        }
+
+        i += 1; /* eat the final paren of this marker */
+
+        let marker_parts = marker.split("x").collect::<Vec<&str>>();
+        let data_block_len = marker_parts[0].parse::<usize>().unwrap();
+        let repeats = marker_parts[1].parse::<usize>().unwrap();
+
+        let marker_range:&str = &input[i .. i+data_block_len];
+        let marker_size = size_of_substring(marker_range.to_string());
+
+        my_size += marker_size * repeats;
+
+        i += data_block_len;
+        //my_size += data_block_len;
+      },
+      _ => {
+        my_size += 1;
+        i += 1;
+      }
+    };
+  }
+
+  my_size
+}
+
+fn part2 (input: String) -> String  {
+  size_of_substring(input).to_string()
 }
 
 pub fn fill() -> super::Day {
@@ -66,5 +114,5 @@ fn test_part1() {
 #[test]
 fn test_part2() {
   let day = fill();
-  assert_eq!((day.part2.run)(day.input.to_string()), "CFLELOYFCS".to_string());
+  assert_eq!((day.part2.run)(day.input.to_string()), "11052855125".to_string());
 }
